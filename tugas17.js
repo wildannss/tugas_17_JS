75c42273df89b50681e58699a6eb266808287d29
wildan <wildan.syarif1021@gmail.com>
2020-03-17 13:24:16 +0700

    tugas niomic

---
+++ tugas17.js
@@ -0,0 +1,13 @@
+function tugas17(){
+    let x = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+    let y = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual"
+    console.log(x.toLowerCase());
+    console.log(x.toUpperCase());
+    console.log(y.toLowerCase());
+    console.log(y.toUpperCase());
+
+    let z = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    console.log(z.charCodeAt(0) + "-" + z.charCodeAt(1) + "-" + z.charCodeAt(2) + "-" + z.charCodeAt(3) + "-" + z.charCodeAt(4) + "-" + z.charCodeAt(5));
+};
+
+tugas17();
